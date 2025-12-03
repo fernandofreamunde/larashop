49f36eb243fc641e484700075958b68f1d507088
Fernando Andrade <fernandofreamunde@gmail.com>
2025-12-03 17:27:28 +0100

    add form input component

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,20 @@
+@props(['label', 'name'])
+<div >
+    <label for={{ $name }} class="block text-sm font-medium text-gray-700">{{ $label }}</label>
+    <div class="mt-2">
+        <input
+            id={{ $name }}
+            @if($name === "email")
+            type="email"
+            @else
+            @elif($name === "password")
+            type="password"
+            @elif($name === "password_confirmation")
+            type="password"
+            @endif
+            name={{ $name }}
+            autocomplete={{ $name }}
+            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        />
+    </div>
+</div>
