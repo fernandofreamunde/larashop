6afd8fad21b494f83717eaa95b60d6f13966fbba
Fernando Andrade <fernandofreamunde@gmail.com>
2025-12-03 17:23:42 +0100

    ✨ add all the needed routes for the application
    add all needed routes for this time of development of the app
    we only allow GETs for now.

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/product/{id}", function () {
+    return view("product");
+});
+
+Route::get("/checkout", function () {
+    return view("checkout");
+});
+
+Route::get("/orders/{id}", function () {
+    return view("order-history-detail");
+});
+
+Route::get("/orders", function () {
+    return view("order-history");
+});
+
+Route::get("/login", function () {
+    return view("login");
+});
+
+Route::get("/register", function () {
+    return view("register");
+});
