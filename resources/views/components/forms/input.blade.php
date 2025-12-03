722cdfb86d94274c5e04e61af3222129854ff157
Fernando Andrade <fernandofreamunde@gmail.com>
2025-12-03 23:26:56 +0100

    fix input component that sets it all as password

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -3,17 +3,16 @@
     <label for={{ $name }} class="block text-sm font-medium text-gray-700">{{ $label }}</label>
     <div class="mt-2">
         <input
-            id={{ $name }}
+            id="{{ $name }}"
             @if($name === "email")
-            type="email"
+                type="email"
+            @elseif($name === "password" || $name === "password_confirmation")
+                type="password"
             @else
-            @elif($name === "password")
-            type="password"
-            @elif($name === "password_confirmation")
-            type="password"
+                type="text"
             @endif
-            name={{ $name }}
-            autocomplete={{ $name }}
+            name="{{ $name }}"
+            autocomplete="{{ $name }}"
             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
         />
     </div>
