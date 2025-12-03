1398784938fb071884f02dd8ef5ba17c3cfc0444
Fernando Andrade <fernandofreamunde@gmail.com>
2025-12-03 18:49:41 +0100

    add Product Model

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $casts = [
+        "price" => "integer",
+    ];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value / 100,
+            set: fn($value) => (int) round((float) $value * 100),
+        );
+    }
+
+    protected function formattedPrice(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => number_format($this->price / 100, 2),
+        );
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,8 +19,39 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            "first_name" => "Test User",
+            "email" => "test@example.com",
+            "isShopkeeper" => true,
+        ]);
+
+        Product::factory()->create([
+            "name" => "Woman's t-shirt",
+            "description" => implode("\n", [
+                "Fancy white t-shirt for girls that love the summer.",
+                ...fake()->paragraphs(3),
+            ]),
+            "image_url" => "/assets/t1g.jpg",
+            "price" => 35.99,
+        ]);
+
+        Product::factory()->create([
+            "name" => "Men's t-shirt",
+            "description" => implode("\n", [
+                "Nice black t-shirt for guys that enjoy to exercise.",
+                ...fake()->paragraphs(3),
+            ]),
+            "image_url" => "/assets/t1.jpg",
+            "price" => 35.99,
+        ]);
+
+        Product::factory()->create([
+            "name" => "Unisex Cap",
+            "description" => implode("\n", [
+                "For that sunny day in the Netherlands summer.",
+                ...fake()->paragraphs(3),
+            ]),
+            "image_url" => "/assets/cap.jpg",
+            "price" => 45.55,
         ]);
     }
 }
