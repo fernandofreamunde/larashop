e7c0ac263f3ec58905a253a5565b60dde2213945
Fernando Andrade <fernandofreamunde@gmail.com>
2025-12-03 19:21:28 +0100

    add cart route and CS FIX

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,31 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [ProductController::class, "index"]);
+Route::get('/', [ProductController::class, 'index']);
 
-Route::get("/product/{product}", [ProductController::class, "show"]);
+Route::get('/product/{product}', [ProductController::class, 'show']);
 
-Route::get("/checkout", function () {
-    return view("checkout");
+Route::post('/cart', [CartController::class, 'store']);
+
+Route::get('/checkout', function () {
+    return view('checkout');
 });
 
-Route::get("/orders/{id}", function () {
-    return view("order-history-detail");
+Route::get('/orders/{id}', function () {
+    return view('order-history-detail');
 });
 
-Route::get("/orders", function () {
-    return view("order-history");
+Route::get('/orders', function () {
+    return view('order-history');
 });
 
-Route::get("/login", function () {
-    return view("login");
+Route::get('/login', function () {
+    return view('login');
 });
 
-Route::get("/register", function () {
-    return view("register");
+Route::get('/register', function () {
+    return view('register');
 });
