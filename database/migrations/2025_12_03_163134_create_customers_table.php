50513eeb8ea1f003ea7a06117b230b2f206004a7
Fernando Andrade <fernandofreamunde@gmail.com>
2025-12-04 00:27:57 +0100

    make company nullable

--- database/migrations/2025_12_03_163134_create_customers_table.php
+++ database/migrations/2025_12_03_163134_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string("email")->unique();
             $table->string("first_name");
             $table->string("last_name");
-            $table->string("company");
+            $table->string("company")->nullable();
             $table->foreignIdFor(User::class)->nullable();
             $table->timestamps();
         });
